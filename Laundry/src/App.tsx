52bc884500b06e98ee4fabb85f9117a7a1a0b411
Nils Ekstrand <ekstrand.nils00@gmail.com>
2024-07-07 15:56:27 +0200

    The start

---
+++ Laundry/src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col" style={{ textAlign: "center" }}>
+          Hello World!
+        </div>
+      </div>
+    </div>
+  );
+}
